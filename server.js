677b466c74f5a138996cb2bf3977ef71ed2da6f6
unknown <akash982jain@gmail.com>
2024-05-01 15:19:53 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http');
+
+// Create a server object
+const server = http.createServer((req, res) => {
+  // Set the response HTTP header with HTTP status and Content type
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  
+  // Send the response body "Hello World"
+  res.end('Sashaktm Server\n');
+});
+
+// Define the port the server will listen on
+const port = 8080;
+
+// Start the server and have it listen on the defined port
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
