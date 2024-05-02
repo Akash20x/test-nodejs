d18408361b5cf1ee87328dc7aaf558744f9d87d5
Akash Jain <46225357+Akash20x@users.noreply.github.com>
2024-05-02 14:17:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   
   // Send the response body "Hello World"
-  res.end('Sashaktm Server\n');
+  res.end('Akash Server\n');
 });
 
 // Define the port the server will listen on
